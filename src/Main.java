c16735c1f20adc469b92d0c2c53583127bbfbb92
ice036 <email@example.com>
2022-06-15 10:29:28 +0800

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
-import com.ky.tools.Files;
+package com.ky.tools;
 import java.io.File;
 import java.util.function.Consumer;
 
+// 请修改 sourcePath 和 destPath 两个字符串的内容
+// 可以直接将路径复制到 sourcePath 和 destPath 中
 public class Main {
 	public static void main(String[] args) {
+		String sourcePath = "C:\\tmp\\修改";
+		String destPath = "C:\\tmp\\修改后";
+		String sourceByReg = sourcePath.replaceAll("\\\\", "\\\\\\\\");
+		String destByReg = destPath.replaceAll("\\\\", "\\\\\\\\");
 		Files.search(
-				new File("D:\\谭浩强  C语言程序设计"),
+				new File(sourcePath),
 				new Consumer<File>() {
 					@Override
 					public void accept(File t) {
@@ -13,16 +19,17 @@ public class Main {
 							Files.codingConverter(
 								t,
 								"GB2312",
-								new File(t.getAbsolutePath().replaceFirst("D:\\\\谭浩强  C语言程序设计", "D:\\dest")),
+								new File(t.getAbsolutePath().replaceFirst(sourceByReg, destByReg)),
 								"UTF-8"
 							);
 						} else { // 不匹配项直接复制，不需要进行任何修改
 							Files.copy(
 								t,
-								new File(t.getAbsolutePath().replaceFirst("D:\\\\谭浩强  C语言程序设计", "D:\\dest"))
+								new File(t.getAbsolutePath().replaceFirst(sourceByReg, destByReg))
 							);
 						}
 					}
 				});
+		System.out.println("程序运行完毕");
 	}
 }
